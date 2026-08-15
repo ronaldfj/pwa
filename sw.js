1824ba5a6ff0bb0989f11ac360e4f9d58741470b
rfranco <ronaldfj@gmail.com>
2026-08-15 14:59:38 -0600

    feat: iteración de usabilidad, onboarding guiado y dashboard unificado de Inicio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fenix-v6-5';
+const CACHE='fenix-v6-7';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(
